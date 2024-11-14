825756db73499e8033a1c9724ce50b19ef3a1138
ThomasGuy <twguy.webdev@gmail.com>
2024-11-14 10:19:11 +0700

    no dashboard

--- sanity.config.js
+++ sanity.config.js
@@ -1,29 +1,15 @@
 // sanity.config.js
 import { defineConfig } from 'sanity'
-import { dashboardTool } from '@sanity/dashboard'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
-import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
 
 export default defineConfig({
   title: 'isolationists',
   projectId: 'ukbaygfd',
   dataset: 'production',
   perspective: 'published',
-  plugins: [structureTool(), visionTool(), dashboardTool({
-    widgets: [
-      netlifyWidget({
-    title: 'My Netlify deploys',
-    sites: [
-      {
-        title: 'Sanity Studio',
-        apiId: '7f1009ce-9a33-4fb1-89f9-313d2f3d3923',
-        buildHookId: 'https://api.netlify.com/build_hooks/66ff71ff0c21d79f004de9f9',
-        name: 'sanity-isolationists-gallery',
-      },
-    ]
-})]})],
+  plugins: [structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
   },
